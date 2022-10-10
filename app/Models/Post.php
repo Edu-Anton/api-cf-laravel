4309df36b0baf57439ffe00993d2a9f6f3c1823e
Eduardo Calderón <eduardo1acc@gmail.com>
2022-10-10 17:19:07 -0500

    Model Relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,4 +11,27 @@ class Post extends Model
 
     const BORRADOR = 1;
     const PUBLICADO = 2;
+
+    // Relación uno a muchos inversa
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
+
+    // Relación muchos a muchos
+    public function tags()
+    {
+        $this->belongsToMany(Tag::class);
+    }
+
+    // Relación uno a muchos polimórfica
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
