4166c9f25760b0a230225cf8fdfe19ccce3bba17
Eduardo Calderón <eduardo1acc@gmail.com>
2022-10-10 17:07:42 -0500

    migrations created

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/prueba', function () {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('/prueba', function(){
     return 'prueba';
 });
+
+Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
