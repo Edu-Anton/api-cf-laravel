1367a165f965dddae20963d2d9be0bde733699e0
Eduardo Calderón <eduardo1acc@gmail.com>
2022-11-20 23:58:37 -0500

    Laravel Passport

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,6 +9,11 @@ use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except(['index', 'show']); 
+    }    
+
     /**
      * Display a listing of the resource.
      *
@@ -33,7 +38,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'name' => 'required|max:255',
             'slug' => 'required|max:255|unique:posts',
             'extract' => 'required',
@@ -42,7 +47,8 @@ class PostController extends Controller
             'user_id' => 'required|exists:users,id',
         ]);
 
-        $post = Post::create($request->all());
+        $data['user_id'] = auth()->user()->id;
+        $post = Post::create($data);
         
         return PostResource::make($post);
     }
